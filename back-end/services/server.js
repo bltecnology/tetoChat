58041618027deeaeefeb1b86280bd52c29540fed
Jean de Castro Aleixo <jean.castroaleixo@gmail.com>
2024-08-06 11:26:01 -0300

    deploy 3

--- back-end/services/server.js
+++ back-end/services/server.js
@@ -30,9 +30,7 @@ app.post("/webhook", async (req, res) => {
   const body = req.body;
   console.log(body)
   if (body){
-    res.send().json(
-      body
-    )
+    res.json({ message: body, message: "teste" });
   }
   // if (body.object === "whatsapp_business_account") {
   //   try {
