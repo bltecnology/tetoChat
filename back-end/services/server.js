f29fdd834357ea6896969d01164763baef3802a0
Jean de Castro Aleixo <jean.castroaleixo@gmail.com>
2024-08-06 16:46:14 -0300

    const connection

--- back-end/services/server.js
+++ back-end/services/server.js
@@ -26,6 +26,21 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+      console.error('Erro ao conectar ao banco de dados:', err);
+      process.exit(1); // Sai do processo em caso de erro na conexão
+  }
+  console.log('Conectado ao banco de dados MySQL.');
+});
+
 app.get('/webhook', function (req, res) {
   if (
       req.query['hub.mode'] == 'subscribe' &&
