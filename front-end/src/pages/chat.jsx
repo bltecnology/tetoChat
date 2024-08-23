32bf87db1e40d1971b4daedd0a20f39fded68917
jean <jean.castroaleixo@gmail.com>
2024-08-23 11:58:23 -0300

    date

--- front-end/src/pages/chat.jsx
+++ front-end/src/pages/chat.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FiPhoneForwarded, FiSmile, FiPaperclip, FiMic, FiMoreVertical } from 'react-icons/fi';
-import { AiOutlineThunderbolt } from 'react-icons/ai'; // Ícone de raio
+import { AiOutlineThunderbolt } from 'react-icons/ai';
 import Header from '../components/header';
-import TransferModal from '../components/modalChat'; // Importando o TransferModal
+import TransferModal from '../components/modalChat';
 import { io } from 'socket.io-client';
 import backgroundImage from '../assets/image.png';
 import EmojiPicker from 'emoji-picker-react';
+import { format } from 'date-fns'; // Importando a função format do date-fns
 
-const socket = io('https://tetochat-8m0r.onrender.com'); // Ajuste para o endereço correto do seu servidor
+const socket = io('https://tetochat-8m0r.onrender.com');
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
@@ -17,24 +18,22 @@ const Chat = () => {
   const [contacts, setContacts] = useState([]);
   const [selectedContact, setSelectedContact] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  const [activeTab, setActiveTab] = useState('chat'); // Para controlar as abas
-  const loggedUser = { id: 1, departmentId: 1 }; // Exemplo de usuário logado (substitua conforme necessário)
+  const [activeTab, setActiveTab] = useState('chat');
+  const loggedUser = { id: 1, departmentId: 1 };
 
-  // Função para adicionar o emoji ao campo de mensagem
   const handleEmojiClick = (event, emojiObject) => {
     if (emojiObject && emojiObject.emoji) {
       setNewMessage(prevInput => prevInput + emojiObject.emoji);
     }
-    setShowEmojiPicker(false); // Fecha o emoji picker após selecionar um emoji
+    setShowEmojiPicker(false);
   };
 
   useEffect(() => {
     const fetchChats = async () => {
       try {
-        // Obtém todos os contatos para os quais o usuário atual enviou mensagens
         const response = await axios.get(`https://tetochat-8m0r.onrender.com/chats`, {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}` // Supondo que o token JWT esteja armazenado no localStorage
+            Authorization: `Bearer ${localStorage.getItem('token')}`
           }
         });
         setContacts(response.data);
@@ -42,7 +41,7 @@ const Chat = () => {
         console.error('Erro ao buscar chats:', error);
       }
     };
-  
+
     const fetchQueue = async () => {
       try {
         const response = await axios.get(`https://tetochat-8m0r.onrender.com/queue?department=${loggedUser.departmentId}`, {
@@ -55,7 +54,7 @@ const Chat = () => {
         console.error('Erro ao buscar fila:', error);
       }
     };
-  
+
     const fetchContacts = async () => {
       try {
         const response = await axios.get('https://tetochat-8m0r.onrender.com/contacts', {
@@ -68,7 +67,7 @@ const Chat = () => {
         console.error('Erro ao buscar contatos:', error);
       }
     };
-  
+
     if (activeTab === 'chat') {
       fetchChats();
     } else if (activeTab === 'fila') {
@@ -77,7 +76,7 @@ const Chat = () => {
       fetchContacts();
     }
   }, [activeTab]);
-  
+
   useEffect(() => {
     if (selectedContact) {
       const fetchMessages = async () => {
@@ -112,7 +111,6 @@ const Chat = () => {
   const handleSendMessage = async () => {
     if (selectedContact && newMessage.trim() !== '') {
       try {
-        // Envia a mensagem para o backend
         const response = await axios.post('https://tetochat-8m0r.onrender.com/send', {
           toPhone: selectedContact.phone,
           text: newMessage,
@@ -121,14 +119,13 @@ const Chat = () => {
             Authorization: `Bearer ${localStorage.getItem('token')}`
           }
         });
-  
+
         if (response.status === 200) {
-          setNewMessage(''); // Limpa o campo de nova mensagem
+          setNewMessage('');
           console.log('Mensagem enviada com sucesso');
-  
-          // Verifique se o contato já está na lista de chats
+
           if (!contacts.find(contact => contact.id === selectedContact.id)) {
-            setContacts([...contacts, selectedContact]); // Adicione o contato à lista de chats
+            setContacts([...contacts, selectedContact]);
           }
         }
       } catch (error) {
@@ -145,30 +142,24 @@ const Chat = () => {
 
   const handleTransferClick = (contact) => {
     setSelectedContact(contact);
-    setShowModal(true); // Ativa o modal
+    setShowModal(true);
   };
 
   const handleTransferComplete = () => {
-    // Filtra o contato transferido da lista de contatos
     setContacts(contacts.filter(contact => contact.id !== selectedContact.id));
-
-    // Depois de transferir o contato, defina o selectedContact como null
     setSelectedContact(null);
-
-    // Fecha o modal após a transferência
     setShowModal(false);
   };
 
   const handleContactClick = (contact) => {
     setSelectedContact(contact);
-    setShowModal(false); // Fecha qualquer modal aberto
+    setShowModal(false);
   };
 
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <div className="flex flex-grow overflow-hidden">
-        {/* Coluna esquerda com as abas de Chat, Fila e Contatos */}
         <div className="flex-shrink-0 w-1/4 bg-white border-r border-gray-200 flex flex-col">
           <div className="flex">
             <button
@@ -208,9 +199,7 @@ const Chat = () => {
             </ul>
           </div>
         </div>
-        {/* Coluna direita com o chat */}
         <div className="flex-grow flex flex-col" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-          {/* Div para exibir o nome do contato */}
           {selectedContact && (
             <div className="w-full p-2 bg-white border-b border-gray-200 flex items-center justify-between">
               <div className="text-lg ml-10 font-bold">{selectedContact.name}</div>
@@ -220,15 +209,16 @@ const Chat = () => {
               </div>
             </div>
           )}
-          {/* Div para exibir as mensagens */}
           <div className="flex-grow p-4 overflow-y-auto">
             {messages.map((message) => (
               <div key={message.id} className={`max-w-xs p-3 my-2 rounded-lg ${message.message_from === 'me' ? 'ml-auto bg-green-200 text-black' : 'mr-auto bg-blue-200 text-black'}`}>
-                {message.message_body}
+                <div>{message.message_body}</div>
+                <div className="text-[10px] text-gray-500 text-right">
+                  {format(new Date(message.message_timestamp * 1000), 'HH:mm')}
+                </div>
               </div>
             ))}
           </div>
-          {/* Div para enviar nova mensagem */}
           <div className="flex items-center p-4 bg-white border-t border-gray-200 relative">
             <button 
               className="p-2 text-gray-500"
@@ -261,10 +251,9 @@ const Chat = () => {
           </div>
         </div>
       </div>
-      {/* Modal de Transferência */}
       {selectedContact && (
         <TransferModal
-          isOpen={showModal} // Passe a prop isOpen em vez de show
+          isOpen={showModal}
           onClose={() => setShowModal(false)}
           onTransfer={handleTransferComplete}
           contactId={selectedContact.id}
