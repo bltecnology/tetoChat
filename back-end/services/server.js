a928be2cdf3c3f63c3bd7b6535bd0133b0147a7d
Jean de Castro Aleixo <jean.castroaleixo@gmail.com>
2024-08-27 00:14:06 -0300

    cors

--- back-end/services/server.js
+++ back-end/services/server.js
@@ -37,13 +37,26 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-  optionsSuccessStatus: 200 // Para garantir o sucesso das requisições preflight em alguns navegadores
+  preflightContinue: false, // Certifique-se de que as requisições preflight não sejam tratadas como erros
+  optionsSuccessStatus: 204 // Responde com sucesso para preflight
 };
 
 app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions)); // Habilitar preflight em todas as rotas
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+
 
 
 const storage = multer.diskStorage({
