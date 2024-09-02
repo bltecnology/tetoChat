02e7f2820a02b4443d5722fd633a6bf276423f77
Jean de Castro Aleixo <jean.castroaleixo@gmail.com>
2024-09-02 16:40:22 -0300

    teste3

--- back-end/services/server.js
+++ back-end/services/server.js
@@ -45,6 +45,7 @@ const corsOptions = {
   credentials: true
 };
 
+
 app.use(cors(corsOptions));
 
 const storage = multer.diskStorage({
@@ -249,62 +250,58 @@ app.post('/send', authenticateJWT, async (req, res) => {
   const userId = req.user.id;
 
   if (!toPhone || !text) {
-    return res.status(400).send("toPhone e text são obrigatórios");
+      return res.status(400).send("toPhone e text são obrigatórios");
   }
 
   try {
-    // Enviando mensagem para a API do WhatsApp
-    await sendMessage(toPhone, text, process.env.WHATSAPP_BUSINESS_ACCOUNT_ID);
+      await sendMessage(toPhone, text, process.env.WHATSAPP_BUSINESS_ACCOUNT_ID);
 
-    // Verificando se o contato já existe no banco de dados
-    const [contactRows] = await pool.query("SELECT id FROM contacts WHERE phone = ?", [toPhone]);
-    let contactId;
-    if (contactRows.length > 0) {
-      contactId = contactRows[0].id;
-    } else {
-      const [result] = await pool.query(
-        "INSERT INTO contacts (name, phone) VALUES (?, ?)",
-        ['API', toPhone]
-      );
-      contactId = result.insertId;
-    }
+      const [contactRows] = await pool.query("SELECT id FROM contacts WHERE phone = ?", [toPhone]);
+      let contactId;
+      if (contactRows.length > 0) {
+          contactId = contactRows[0].id;
+      } else {
+          const [result] = await pool.query(
+              "INSERT INTO contacts (name, phone) VALUES (?, ?)",
+              ['API', toPhone]
+          );
+          contactId = result.insertId;
+      }
 
-    // Inserindo a mensagem na tabela whatsapp_messages
-    const sql = `
-      INSERT INTO whatsapp_messages (phone_number_id, display_phone_number, contact_name, wa_id, message_id, message_from, message_timestamp, message_type, message_body, contact_id, user_id)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-    const values = [
-      process.env.WHATSAPP_BUSINESS_ACCOUNT_ID,
-      process.env.DISPLAY_PHONE_NUMBER,
-      'API',
-      toPhone,
-      `msg-${Date.now()}`,
-      'me',
-      Math.floor(Date.now() / 1000).toString(),
-      'text',
-      text,
-      contactId,
-      userId
-    ];
+      const sql = `
+        INSERT INTO whatsapp_messages (phone_number_id, display_phone_number, contact_name, wa_id, message_id, message_from, message_timestamp, message_type, message_body, contact_id, user_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+      const values = [
+          process.env.WHATSAPP_BUSINESS_ACCOUNT_ID,
+          process.env.DISPLAY_PHONE_NUMBER,
+          'API',
+          toPhone,
+          `msg-${Date.now()}`,
+          'me',
+          Math.floor(Date.now() / 1000).toString(),
+          'text',
+          text,
+          contactId,
+          userId
+      ];
 
-    await pool.query(sql, values);
+      await pool.query(sql, values);
 
-    // Inserindo a conversa na tabela de chat do usuário
-    const chatTableName = `chat_user_${userId}`;
-    const insertChatQuery = `
-      INSERT INTO ${chatTableName} (contact_id, conversation_id)
-      VALUES (?, ?)
-    `;
-    await pool.query(insertChatQuery, [contactId, contactId]);
+      // Inserir a conversa na tabela de chat do usuário
+      const chatTableName = `chat_user_${userId}`;
+      const insertChatQuery = `
+          INSERT INTO ${chatTableName} (contact_id, conversation_id)
+          VALUES (?, ?)
+      `;
+      await pool.query(insertChatQuery, [contactId, contactId]);
 
-    res.status(200).send("Mensagem enviada com sucesso");
+      res.status(200).send("Mensagem enviada com sucesso");
   } catch (error) {
-    console.error("Erro ao enviar mensagem:", error.message, error.stack);
-    res.status(500).send("Erro ao enviar mensagem");
+      console.error("Erro ao enviar mensagem:", error);
+      res.status(500).send("Erro ao enviar mensagem");
   }
 });
-
 
 
 app.get("/chats", authenticateJWT, async (req, res) => {
