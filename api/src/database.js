114ae802d82eeca89de3df30c335e84910b72c74
jean <jean.castroaleixo@gmail.com>
2024-10-02 10:55:29 -0300

    db1

--- api/src/database.js
+++ api/src/database.js
@@ -12,7 +12,7 @@ dotenv.config();
 const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 3305,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
