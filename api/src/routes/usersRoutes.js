ef8a507c1bd63b36724103886e14a7f2e3d623d3
jean <jean.castroaleixo@gmail.com>
2024-10-10 10:56:47 -0300

    addUser

--- api/src/routes/usersRoutes.js
+++ api/src/routes/usersRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers, updateUser } from "../controllers/usersController.js";
+import { addUser, getUsers, updateUser } from "../controllers/usersController.js";
 
 const usersRoutes = express.Router();
 
 usersRoutes.get("/users", getUsers);
+usersRoutes.post("/users", addUser);
 usersRoutes.put("/users/:id", updateUser);
 
 export { usersRoutes };
