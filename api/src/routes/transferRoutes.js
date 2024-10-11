a8b7b22609a836f1680d722d2a00b440eb3a78fa
jean <jean.castroaleixo@gmail.com>
2024-10-11 14:17:37 -0300

    transfer2

--- api/src/routes/transferRoutes.js
+++ api/src/routes/transferRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { transfer } from "../controllers/transferController.js";
 
-const queueRoutes = express.Router();
+const transferRoutes = express.Router();
 
 
 transferRoutes.post("/transfer", transfer);
