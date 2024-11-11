783b23187ec779d2a887abebf9c61f1c92c32c57
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-11-11 15:33:52 -0300

    TESTE

--- api/src/controllers/messagesController.js
+++ api/src/controllers/messagesController.js
@@ -319,7 +319,10 @@ export const receiveMessage = async (request, response) => {
         }
       }
     }
-
+    
+    
+    console.log("AQUI")
+    
     const bodyBotMessage = `Olá ${contact.profile.name}! Seja muito bem-vindo(a) ao atendimento digital da Teto Bello. Para direcioná-lo, selecione uma opção abaixo:\n\n1 - Comercial / Vendas\n2 - Instalação / Assistência Técnica\n3 - Financeiro / Adm\n4 - Projetos\n5 - Compras\n6 - Trabalhe Conosco`;
     
     console.log("AQUI")
