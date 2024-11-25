386674f0f234335ea0c10572ab4e6e74f9f0f8d0
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-11-25 11:36:38 -0300

    change update pfp route

--- api/src/routes/usersRoutes.js
+++ api/src/routes/usersRoutes.js
@@ -7,7 +7,7 @@ const usersRoutes = express.Router();
 usersRoutes.get("/users", getUsers);
 usersRoutes.post("/users", addUser);
 usersRoutes.put("/users/:id", updateUser);
-usersRoutes.post("/updateProfilePicture", upload.single("file"), updateProfilePicture);
+usersRoutes.post("/v1/settings/profile/photo", upload.single("file"), updateProfilePicture);
 
 
 export { usersRoutes };
