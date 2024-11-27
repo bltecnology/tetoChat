24cf2ce0f52bd93c3e831e6ce8c2bc7b0c059f88
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-11-27 14:46:45 -0300

    change cors

--- api/src/server.js
+++ api/src/server.js
@@ -24,7 +24,7 @@ const io = new Server(server, {
     origin: [
       "https://teto-chat.vercel.app", // Corrected protocol
       "http://localhost:5173",
-      "https://tetochat-pgus.onrender.com",
+      "https://tetochat-backend.onrender.com",
       "https://teto-chat-7fsq1058t-bltecnologys-projects.vercel.app",
       "https://tetochat-svsn.onrender.com"
     ],
@@ -40,7 +40,7 @@ app.use(cors({
   origin: [
     "https://teto-chat.vercel.app", // Corrected protocol
     "http://localhost:5173",
-    "https://tetochat-pgus.onrender.com",
+    "https://tetochat-backend.onrender.com",
     "https://teto-chat-7fsq1058t-bltecnologys-projects.vercel.app",
     "https://tetochat-svsn.onrender.com"
   ],
