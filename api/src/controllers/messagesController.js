b4f8f0998e29af657b65cefdbc5e65d86c587c61
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-12-04 11:14:40 -0300

    bugfix new contact

--- api/src/controllers/messagesController.js
+++ api/src/controllers/messagesController.js
@@ -229,12 +229,10 @@ export const receiveMessage = async (request, response) => {
             if (contactRows.length > 0) {
               contactId = contactRows[0].id;
             } else {
-              const profilePictureUrl = contact?.profile?.profile_picture?.url || null;
               const insertQuery = "INSERT INTO contacts (name, phone) VALUES (?, ?)";
               const insertValues = ["API", contact.wa_id];
               const [result] = await pool.query(insertQuery, insertValues);
               contactId = result.insertId;
-              isNewContact = true;
             }
           } catch (err) {
             console.error("Erro ao buscar ou criar contato:", err);
