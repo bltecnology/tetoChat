2793e884bc68dd9e75ac7565f646eea1ca27b6be
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-12-10 14:28:48 -0300

    Change update user

--- api/src/controllers/usersController.js
+++ api/src/controllers/usersController.js
@@ -35,12 +35,11 @@ export const updateUser = async (req, res) => {
   const userId = req.params.id;
   const { name, email, password, position_id, department_id } = req.body;
 
-  if (!name || !email || !password || !position_id || !department_id) {
+  if (!name || !email || !position_id || !department_id) {
     return res.status(400).send('Todos os campos são obrigatórios');
   }
 
   try {
-
     const [departmentRow] = await pool.query("SELECT id FROM departments WHERE id = ?", [department_id]);
     if (departmentRow.length === 0) {
       return res.status(404).send('Departamento não encontrado');
@@ -60,18 +59,29 @@ export const updateUser = async (req, res) => {
       return res.status(404).send("Usuário não encontrado");
     }
 
-    const updatedUser = {
-      name: name || user[0].name,
-      email: email || user[0].email,
-      position_id: position_id || user[0].position_id,
-      department_id: department_id || user[0].department_id,
-    };
+    let updatedUser;
 
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    if (password) {
+    if (password){
+      updatedUser = {
+        name: name || user[0].name,
+        email: email || user[0].email,
+        position_id: position_id || user[0].position_id,
+        department_id: department_id || user[0].department_id,
+      };
+  
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+  
       updatedUser.password = hashedPassword;
+    } else {
+      updatedUser = {
+        name: name || user[0].name,
+        email: email || user[0].email,
+        position_id: position_id || user[0].position_id,
+        department_id: department_id || user[0].department_id,
+      };
     }
+
+    
 
     await pool.query("UPDATE users SET ? WHERE id = ?", [updatedUser, userId]);
 
