0e3e7945b8d37c2720bc8aecd4635799cd2eba1f
JhonatanSampaioF <jhonatan.sampaiof@gmail.com>
2024-12-10 13:59:45 -0300

    Remove auth update dep

--- api/src/routes/departmentsRoutes.js
+++ api/src/routes/departmentsRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { addDepartment, deleteDepartment, getDepartments, updateDepartment } from "../controllers/departmentsController.js";
-import { authenticateJWT } from "../models/auth.js";
 
 const departmentsRoutes = express.Router();
 
 departmentsRoutes.get("/departments", getDepartments);
 departmentsRoutes.post("/departments", addDepartment);
 departmentsRoutes.delete("/departments/:id", deleteDepartment);
-departmentsRoutes.put("/departments/:id", updateDepartment, authenticateJWT);
+departmentsRoutes.put("/departments/:id", updateDepartment);
 
 export { departmentsRoutes };
